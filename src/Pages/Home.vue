8b5027e12456cd23d435193130e9e03006d81b78
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-05-05 17:42:12 -0300

    Detalhes

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import CardPoke from '@/components/CardPoke.vue';
 import {Input} from '@/components/ui/input';
+import {Search} from 'lucide-vue-next';
 
 
 const pokemons = ref([]);
@@ -11,22 +12,33 @@ const router = useRouter();
 const loading = ref(false);
 
 onMounted(async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
-  const data = await res.json();
   try {
     loading.value = true;
-    pokemons.value = data.results.map((pokemon) => {
-    const id = pokemon.url.split('/').filter(Boolean).pop();
-    return {
-      ...pokemon,
-      id,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
-    };
-    });
+
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const data = await res.json();
+
+    // Buscar detalhes para cada Pokémon
+    const detalhes = await Promise.all(
+      data.results.map(async (pokemon) => {
+        const resp = await fetch(pokemon.url);
+        const detalhesPokemon = await resp.json();
+
+        return {
+          id: detalhesPokemon.id,
+          name: detalhesPokemon.name,
+          image: detalhesPokemon.sprites.other['official-artwork'].front_default,
+          types: detalhesPokemon.types, 
+        };
+      })
+    );
+
+    pokemons.value = detalhes;
   } finally {
     loading.value = false;
   }
 });
+
 
 const pokemonsFiltrados = computed(() => {
   if (!procurarPokemon.value) return pokemons.value;
@@ -42,13 +54,23 @@ const verDetalhes = (pokemon) => {
 
 <template>
   <main class="max-w-7xl px-4 mx-auto py-6">
+<div class="flex items-center gap-3 mb-4">
+  <img class="w-12 h-12 animate__rotateIn animate__infinite	infinite animate_animated  " src="../assets/pokeboll.png" alt="Logo">
+  <h1 class="text-4xl font-bold">Pokédex</h1>
+</div>
+    <p class="mb-4 text-zinc-800 text-md">Encontre seus Pokémon favoritos</p>
 
-    <h1 class="text-4xl font-bold mb-4">Pokédex</h1>
-    <Input
-      v-model="procurarPokemon"
-      placeholder="Buscar Pokémon"
-      class="mb-4"
-    />
+    <div class="relative w-full mb-6 items-center">
+    <Input 
+    v-model="procurarPokemon"
+    id="search" 
+    type="text" 
+    placeholder="Digite o nome do Pokémon" 
+    class="pl-10 py-5 " />
+    <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+      <Search class="size-6 text-muted-foreground" />
+    </span>
+  </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <CardPoke
